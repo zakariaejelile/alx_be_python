9f8c3660a91b83061c970bd2035e1719e51ef5f7
jelile zakariae <jelilezakariae@gmail.com>
2025-09-21 16:06:36 -0700

    222

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum = int(number1) + int(number2)
+subtract = int(number1) - int(number2)
+product = int(number1) * int(number2)
+
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Substraction of {number2} and {number1} is {subtract}")
+print(f"Multiplication of {number1} and {number2} is {product}")
