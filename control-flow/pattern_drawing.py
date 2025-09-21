c4646656142ef8a33064c8ecfe36e6b7ee61e9d4
jelile zakariae <jelilezakariae@gmail.com>
2025-09-21 15:51:33 -0700

    updating drawing pattern file

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,14 @@
 num = int(input("Enter the size of the pattern:"))
 
-for i in range(num):
-    for j in range(num):
+while i in range(num):
+    while j in range(num):
         print("*", end=" ")
     print()
+
+
+
+
+
+
+
 
